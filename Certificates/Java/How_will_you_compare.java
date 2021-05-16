b89c6f1b2c3aca0202f4e2a0197d05d98288d329
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2021-05-16 19:02:48 +0530

    Create How_will_you_compare.java

---
+++ Certificates/Java/How_will_you_compare.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Comparator{
+    boolean compare(int a, int b){
+        if(a==b)
+            return true;
+        else
+            return false;
+        }
+    boolean compare(String a, String b){
+        if(a.compareTo(b) == 0)
+            return true;
+        else
+            return false;
+        }
+    boolean compare(int a[], int b[]){
+        if(a.length != b.length)
+            return false;
+        else{
+            for(int i = 0;i<a.length;i++)
+            {
+                if(a[i] != b[i])
+                    return false;
+            
+            }
+            return true;
+        }
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Comparator comp = new Comparator();
+        
+        int testCases = Integer.parseInt(scan.nextLine());
+        while(testCases-- > 0){
+            int condition = Integer.parseInt(scan.nextLine());
+            switch(condition){
+                case 1:
+                    String s1=scan.nextLine().trim();
+                    String s2=scan.nextLine().trim();
+                    
+                    System.out.println( (comp.compare(s1,s2)) ? "Same" : "Different" );
+                    break;
+                case 2:
+                    int num1 = scan.nextInt();
+                    int num2 = scan.nextInt();
+                    
+                    System.out.println( (comp.compare(num1,num2)) ? "Same" : "Different");
+                    if(scan.hasNext()){ // avoid exception if this last test case
+                        scan.nextLine(); // eat space until next line
+                    }
+                    break;
+                case 3:
+                    // create and fill arrays
+                    int[] array1 = new int[scan.nextInt()];
+                    int[] array2 = new int[scan.nextInt()];
+                    for(int i = 0; i < array1.length; i++){
+                        array1[i] = scan.nextInt();
+                    }
+                    for(int i = 0; i < array2.length; i++){
+                        array2[i] = scan.nextInt();
+                    }
+                    
+                    System.out.println( comp.compare(array1, array2) ? "Same" : "Different");
+                    if(scan.hasNext()){ // avoid exception if this last test case
+                        scan.nextLine(); // eat space until next line
+                    }
+                    break;
+                default:
+                    System.err.println("Invalid input.");
+            }// end switch
+        }// end while
+        scan.close();
+    }
+}
